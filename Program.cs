edd20c8e6e3112bd9f4cb3d5c8a278b6050ff07f
M_J_O_N_E_S <M_J_O_N_E_S@hotmail.com>
2013-09-30 22:25:34 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace CustomFormatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var formatProvider = new WeekdayFormatProvider();
+            
+            var formattedMinDate = string.Format(formatProvider, "{0}", DateTime.MinValue);
+            Console.WriteLine(formattedMinDate);
+            
+            var formattedMinDateReversed = string.Format(formatProvider, "{0:R}", DateTime.MinValue);
+            Console.WriteLine(formattedMinDateReversed);
+
+            var formattedInteger = string.Format(formatProvider, "{0:R} {1}", DateTime.MinValue, int.MaxValue);
+            Console.WriteLine(formattedInteger);
+            
+            Console.ReadLine();
+        }
+    }
+
+    public class WeekdayFormatProvider : IFormatProvider
+    {
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+            {
+                return new WeekdayFormatter();
+            }
+            return null;
+        }
+    }
+
+    public class WeekdayFormatter : ICustomFormatter
+    {
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            try
+            {
+                var dayName = ((DateTime)arg).DayOfWeek.ToString();
+                if (format == "R")
+                {
+                    return new string(dayName.Reverse().ToArray());
+                }
+                return dayName;
+            }
+            catch (InvalidCastException)
+            {
+                if (arg is IFormattable)
+                {
+                    return ((IFormattable)arg).ToString(format, CultureInfo.CurrentCulture);
+                }
+                else if (arg != null)
+                {
+                    return arg.ToString();
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+[assembly: AssemblyTitle("CustomFormatter")]
+[assembly: AssemblyCompany("M_J_O_N_E_S")]
+[assembly: AssemblyProduct("CustomFormatter")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
